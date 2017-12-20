f12ea53822a39233f1ebb7d10a155be0181bfc12
plough <plough@baimoz.me>
2017-12-20 16:11:28 +0800

    完成 0008 练习

---
+++ 0008/answer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from bs4 import BeautifulSoup
+
+def main():
+    with open('index.html') as f:
+        soup = BeautifulSoup(f.read(), 'html.parser')
+    print(soup.body)
+
+
+if __name__ == "__main__":
+    main()
